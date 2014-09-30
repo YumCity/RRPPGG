532b7656f6060c5113ba93b9bd3d1dcff3094dde
yiwan <caoli5288@gmail.com>
2014-09-30 22:19:18 +0800

    Update SimWork.

--- SimWorker/src/com/mengcraft/rpg/SimWorker.java
+++ SimWorker/src/com/mengcraft/rpg/SimWorker.java
@@ -1,17 +1,21 @@
 package com.mengcraft.rpg;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.mcstats.Metrics;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -27,10 +31,14 @@ public class SimWorker extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        new Thread(new MetricsThread(this)).start();
         new SendAdvert().runTaskLater(this, 120);
         WorkerListener listener = new WorkerListener();
-        Bukkit.getServer().getPluginManager().registerEvents(listener, this);
-        listener.runTaskTimer(this, 600, 600);
+        getServer().getPluginManager().registerEvents(listener, this);
+        getServer().getScheduler().runTaskTimer(this, listener
+                , getConfig().getInt("Config.TimeOutByTick", 1200)
+                , getConfig().getInt("Config.TimeOutByTick", 1200)
+        );
     }
 
     private class SendAdvert extends BukkitRunnable {
@@ -40,83 +48,83 @@ public class SimWorker extends JavaPlugin {
                     ChatColor.GREEN + "梦梦家高性能服务器出租"
                     , ChatColor.GREEN + "淘宝店 http://shop105595113.taobao.com"
             };
-            Bukkit.getConsoleSender().sendMessage(strings);
+            getServer().getConsoleSender().sendMessage(strings);
         }
     }
 
-    private class WorkerListener extends BukkitRunnable implements Listener {
+    private class WorkerListener implements Listener, Runnable {
 
-        private final HashMap<String, Integer> pointMap;
+        private final HashMap<String, Integer> deathMap;
 
         public WorkerListener() {
-            pointMap = new HashMap<>();
+            this.deathMap = new HashMap<String, Integer>();
         }
 
         @Override
         public void run() {
-            for (String key : pointMap.keySet()) {
-                if (pointMap.get(key) > getConfig().getInt("Config.ActTime", 30)) {
-                    effect(key);
-                }
-            }
-            pointMap.clear();
-        }
-
-        private void effect(String key) {
-            Player player = getServer().getPlayerExact(key);
-            if (player.isOnline() && player.getFoodLevel() > 1) {
+            for (Player player : getServer().getOnlinePlayers()) {
                 player.setFoodLevel(player.getFoodLevel() - 1);
-            }
-        }
-
-        private void addPoint(String name) {
-            Integer value = pointMap.get(name);
-            if (value != null) {
-                value = value + 1;
-                pointMap.put(name, value);
-            } else {
-                pointMap.put(name, 1);
             }
         }
 
         @EventHandler
         private void onReSpawn(PlayerRespawnEvent event) {
-            event.getPlayer().setFoodLevel(4);
+            Integer food = deathMap.remove(event.getPlayer().getName());
+            if (food != null) {
+                event.getPlayer().setFoodLevel(food > 0 ? food : 1);
+                event.getPlayer().setHealth(getConfig().getDouble("Config.ReSpawnHealth", 5));
+            }
         }
 
-        @EventHandler
+        @EventHandler(priority = EventPriority.MONITOR)
+        public void onDead(PlayerDeathEvent event) {
+            if (event.getEntity().isDead()) {
+                deathMap.put(event.getEntity().getName(), event.getEntity().getFoodLevel());
+            }
+        }
+
+        @EventHandler(ignoreCancelled = true)
         private void onAttack(EntityDamageByEntityEvent event) {
             if (event.getDamager() instanceof Player) {
                 Player attacker = (Player) event.getDamager();
-                addPoint(attacker.getName());
                 event.setDamage(event.getDamage() * attacker.getFoodLevel() / 20);
             }
             if (event.getEntity() instanceof Player) {
                 Player entity = (Player) event.getEntity();
-                addPoint(entity.getName());
                 event.setDamage(event.getDamage() * (2 - entity.getFoodLevel() / 20));
             }
         }
 
-        @EventHandler
+        @EventHandler(ignoreCancelled = true)
         private void onMine(BlockBreakEvent event) {
-            if (event.getPlayer().getFoodLevel() > getConfig().getInt("Config.LowFood", 5)) {
-                String name = event.getPlayer().getName();
-                addPoint(name);
-            } else {
+            if (event.getPlayer().getFoodLevel() < getConfig().getInt("Config.LowFoodsLevel", 5)) {
                 event.getPlayer().sendMessage(ChatColor.RED + "你已经没力气破坏方块了");
                 event.setCancelled(true);
             }
         }
 
-        @EventHandler
+        @EventHandler(ignoreCancelled = true)
         private void onPlace(BlockPlaceEvent event) {
-            if (event.getPlayer().getFoodLevel() > getConfig().getInt("Config.LowFood", 5)) {
-                String name = event.getPlayer().getName();
-                addPoint(name);
-            } else {
+            if (event.getPlayer().getFoodLevel() < getConfig().getInt("Config.LowFoodsLevel", 5)) {
                 event.getPlayer().sendMessage(ChatColor.RED + "你已经没力气放置方块了");
                 event.setCancelled(true);
+            }
+        }
+    }
+
+    private class MetricsThread implements Runnable {
+        private final Plugin plugin;
+
+        public MetricsThread(Plugin plugin) {
+            this.plugin = plugin;
+        }
+
+        @Override
+        public void run() {
+            try {
+                new Metrics(plugin).start();
+            } catch (IOException e) {
+                getLogger().warning("Can not link to Metrics server!");
             }
         }
     }
